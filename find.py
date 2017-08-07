216dd6f615d7acb7d899b538287c4cc2ea76463c
Ian Miell <ian.miell@gmail.com>
2017-08-07 20:30:49 +0100

    find

--- find.py
+++ find.py
@@ -2,6 +2,8 @@
 import beggarmypython
 import random
 import string
+import matplotlib.pyplot as plt
+import numpy as np
 
 deck = 36*list('-') + 4*list('K') + 4*list('A') + 4*list('Q') + 4*list('J')
 
@@ -9,20 +11,27 @@ bestsofar = 0
 bestdecksofar = deck
 count = 0
 results = {}
+results_list = []
 while True:
 	count += 1
 	random.shuffle(deck)
 	res = beggarmypython.play((string.join(deck[:26],''),string.join(deck[26:],'')), verbose=False)
+	results_list = results_list + [res]
 	if res > bestsofar:
 		bestsofar = res
 		bestdecksofar = deck
 		print bestsofar
 		print bestdecksofar
-	else:
-		try:
-			results[res] += 1
-		except:
-			results[res] = 1
-	if count % 1000000 == 1:
+	try:
+		results[res] += 1
+	except:
+		results[res] = 1
+	if count % 1000 == 1:
 		print count
 		print results
+		print bestsofar
+		print bestdecksofar
+		a = np.hstack((results_list))
+		plt.hist(a,bins='auto')
+#>>> plt.title("Histogram with 'auto' bins")
+		plt.show()
